39222103cd0b82e24a576186062056cd4b9a8eec
Natalia Flores <florn640@newschool.edu>
2016-02-23 15:54:36 +0000

    added details

--- story/main.js
+++ story/main.js
@@ -1,8 +1,6 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-    console.log( "ready!" );
     $('.flip').click(function() {
-        console.log("replace image");
         if ($(this).hasClass( "active" )) {
             $(this).removeClass( "active" );
         } else {
